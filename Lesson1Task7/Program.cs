080828e8d8448f28932cf9fcb748d72d3460ce91
grach727 <grach727@internet.ru>
2023-09-09 19:51:30 +0300

    Solution from lesson 1 last task

---
+++ Lesson1Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = int.Parse(Console.ReadLine()??"0");
+
+if (num >= 100 && num < 1000)
+{
+    int lastDigit = num % 10;
+    Console.WriteLine("Last digit of this number is " + lastDigit);
+}
+else
+{
+    Console.WriteLine("This number is not a 3-digit number!");
+}
